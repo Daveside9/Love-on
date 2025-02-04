b670b60a08a9ffe980e038fe9d5f9720dff9919a
joel david <daveside00468@gmail.com>
2025-02-04 14:22:55 +0100

    all commit

--- valentine-booking/src/components/home.js
+++ valentine-booking/src/components/home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/home.css";
-import StarRating from './StarRating'; // Import the StarRating component
 
 function Home() {
   return (
@@ -12,20 +11,10 @@ function Home() {
 
       <div className="text-container">
         <h1>Living Spring Loveworld</h1>
-        <p></p>
+        <p>Celebrate love this Valentine’s Day with a serene, romantic picnic experience. Book your private space for an unforgettable moment together in a peaceful atmosphere designed just for lovers</p>
         <Link to="/tour">
           <button className="tour-btn">Take a Tour</button>
         </Link>
-      </div>
-
-      <div className="romantic-story">
-        <p></p>
-      </div>
-
-      {/* Star Rating Section */}
-      <div className="star-rating-container">
-        <h2>Your Feedback Matters!</h2>
-        <StarRating /> {/* Display Star Rating Component */}
       </div>
 
       <div className="contact-us">
