7b330df257798cc94fc26b3929a3ef1b3bd9aa94
joel david <daveside00468@gmail.com>
2025-02-05 10:09:42 +0100

    updated

--- valentine-booking/src/components/tour.js
+++ valentine-booking/src/components/tour.js
@@ -7,20 +7,19 @@ function Tour() {
     <div className="tour-container">
       <h1>Welcome to the Valentine's Day gift packs!</h1>
 
-      {/* Your Google Form embedded here */}
-      <section className="tour-content">
-        <iframe
-          src="https://docs.google.com/forms/d/e/1FAIpQLSfWA9C17DjdeYqppQuYgivL3B3R0eFdHgsdXFdPLb8n6hH5pA/viewform?embedded=true"
-          width="640"
-          height="2416"
-          frameborder="0"
-          marginheight="0"
-          marginwidth="0"
-          title="Valentine's Day Gift Packs Form"
-        >
-          Loading…
-        </iframe>
-      </section>
+      <section className="tour-content" style={{ maxWidth: '640px', margin: '0 auto' }}>
+  <iframe
+    src="https://docs.google.com/forms/d/e/1FAIpQLSfWA9C17DjdeYqppQuYgivL3B3R0eFdHgsdXFdPLb8n6hH5pA/viewform?embedded=true"
+    width="100%" 
+    height="1000"  // Adjust the height as needed based on your form's content
+    frameBorder="0"
+    marginHeight="0"
+    marginWidth="0"
+    title="Valentine's Day Gift Packs Form"
+  >
+    Loading…
+  </iframe>
+</section>
 
       {/* Home button to navigate back to the homepage */}
       <div className="home-button-container">
